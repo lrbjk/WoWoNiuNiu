9ee5170a5e91614ec85f1202e76de275eb2dc513
MagiCooola <MGCA0110@163.com>
2024-10-31 15:24:28 +0800

    进入游戏时会根据当前2D/3D判断阴影

--- WoWoNiuNiu/Assets/RainbowLii/Camera/CameraController.cs
+++ WoWoNiuNiu/Assets/RainbowLii/Camera/CameraController.cs
@@ -15,6 +15,32 @@ public class CameraController : MonoBehaviour
     {
        
         lights = FindObjectsOfType<Light>();  
+        if (is2DCamera)
+        {
+            foreach (Light light in lights)
+            {
+                light.shadows = LightShadows.Hard;
+            }
+            //open shadow when 3D
+            twoD_Camera.Priority = 0;
+            threeD_Camera.Priority = 10;
+            is2DCamera = false;
+            StaticData.is2DCamera = false;
+            mainCamera.orthographic = false;
+        }
+        else
+        {
+            foreach (Light light in lights)
+            {
+                light.shadows = LightShadows.None;
+            }
+            //close shadow when 2D
+            threeD_Camera.Priority = 0;
+            twoD_Camera.Priority = 10;
+            is2DCamera = true;
+            StaticData.is2DCamera = true;
+            //mainCamera.orthographic = true;
+        }
         //get all light in this scene
     }
 
